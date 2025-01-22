c7db14c4c77fe4ded66c39bab6e256249bf22b99
Ilya Mikhaltsou <morpheby@gmail.com>
2025-01-22 21:12:42 +0300

    Added option to poll for latest temperature value without implicit conversion

--- src/DS18B20.cpp
+++ src/DS18B20.cpp
@@ -89,6 +89,33 @@ float DS18B20::getTempC() {
     return temp / 16.0;
 }
 
+float DS18B20::getLastTempC() {
+    readScratchpad();
+    uint8_t lsb = selectedScratchpad[TEMP_LSB];
+    uint8_t msb = selectedScratchpad[TEMP_MSB];
+
+    switch (selectedResolution) {
+        case 9:
+            lsb &= 0xF8;
+            break;
+        case 10:
+            lsb &= 0xFC;
+            break;
+        case 11:
+            lsb &= 0xFE;
+            break;
+    }
+
+    uint8_t sign = msb & 0x80;
+    int16_t temp = (msb << 8) + lsb;
+
+    if (sign) {
+        temp = ((temp ^ 0xffff) + 1) * -1;
+    }
+
+    return temp / 16.0;
+}
+
 float DS18B20::getTempF() {
     return getTempC() * 1.8 + 32;
 }
